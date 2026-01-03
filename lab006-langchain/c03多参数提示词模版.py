dffe55f502055ce540cb3095a1ce4428935c6789
tangfire <52628216@qq.com>
2026-01-03 16:01:53 +0800

    langchain实战学习

--- lab006-langchain/c03多参数提示词模版.py
+++ lab006-langchain/c03多参数提示词模版.py
@@ -7,11 +7,11 @@ from langchain_core.prompts import ChatPromptTemplate
 
 prompt = ChatPromptTemplate.from_messages([
     ("system", "You are a helpful assistant."),
-    ("human", "写一首描述{sence}的诗")
+    ("human", "写一首描述{sence}的诗,格式要求为{type}")
 ])
 
 # 格式化
-formatted = prompt.format(sence="春天")
+formatted = prompt.format(sence="春天",type="五言律诗")
 
 
 model = ChatOpenAI(
